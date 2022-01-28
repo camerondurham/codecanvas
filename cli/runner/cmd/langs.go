274cc81d8cb24d77cc6acaaf5ad2044331b668b5
Arek Ouzounian <agouzo777@gmail.com>
2022-01-28 01:13:07 -0800

    Added CLI GET request for language from mock API; it's far from being perfect but it's currently working at least. These are not only my first lines of Go, but my first Go commit, so go Go

--- cli/runner/cmd/langs.go
+++ cli/runner/cmd/langs.go
@@ -5,10 +5,26 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
+
+const (
+	LANG_ENDPOINT = "/api/v1/languages"
+)
+
+var (
+	server string = "http://localhost:8080"
+)
+
+// Simple struct to hold JSON vals from API call
+type Langs struct {
+	Languages []string `json:"languages"`
+}
 
 // langsCmd represents the langs command
 var langsCmd = &cobra.Command{
@@ -16,7 +32,33 @@ var langsCmd = &cobra.Command{
 	Short: "query the server for supported languages",
 	Run: func(cmd *cobra.Command, args []string) {
 		// implement CLI subcommand logic here
-		fmt.Println("langs called")
+
+		// later on, I should prob use an HTTP client to craft requests
+		//semi-hardcoded url here; any better way to dynamically grab api path?
+		resp, err := http.Get(server + LANG_ENDPOINT)
+		if err != nil {
+			fmt.Println("Bad API call")
+			return
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		//all of these error checks get pretty redundant,
+		//maybe there's a way to make this better in the future
+		if resp.StatusCode > 299 {
+			fmt.Printf("Response failed with status code: %d\n", resp.StatusCode)
+			return
+		}
+		if err != nil {
+			fmt.Printf(err.Error())
+			return
+		}
+		var res Langs
+		fmt.Print("Currently supported languages: ")
+		json.Unmarshal(body, &res)
+		for _, lang := range res.Languages {
+			fmt.Printf("%s ", lang)
+		}
+		//fmt.Println("langs called")
 	},
 }
 
