c80cf27531ae555bd78a636e23c0bb20be53f41a
Cameron Durham <u64.cam@gmail.com>
2022-01-30 13:22:10 -0700

    use existing action instead

--- hack/check_unformatted.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x
-
-# go to repository root, handle case where this is run in root directory
-cd $(git rev-parse --show-toplevel)
-
-unformatted_code=$(go fmt ./...)
-[ -n "$unformatted_code" ] && echo "found unformatted code in repository, exiting..." && exit 1
