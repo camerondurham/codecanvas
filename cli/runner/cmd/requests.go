ec60b664376627247e659c27e054275d4444f931
Arek Ouzounian <agouzo777@gmail.com>
2022-01-29 16:39:33 -1000

    Cleaned up getLangListJSON() and added a bit of documentation.

--- cli/runner/cmd/requests.go
+++ cli/runner/cmd/requests.go
@@ -1,3 +1,5 @@
+//file to contain helper functions for different commands
+
 package cmd
 
 import (
@@ -7,34 +9,37 @@ import (
 	"net/http"
 )
 
-//file to contain helper functions for different commands
-
 // Simple struct to hold JSON vals from API call
 type Langs struct {
 	Languages []string `json:"languages"`
-	Error     bool
 }
 
+// This function is designed to send an API call to the specified endpoint.
+// The server parameter is designed to take in a URL, such as 'http://localhost:8080'.
+// The endpoint parameter is designed to take in the proper API endpoint, such as '/api/v1/languages'.
+// This function will return a pointer to the Langs struct which contains a list of the languages
+// currently supported by the API, and an error parameter in the case of failure.
 func getLangListJSON(server string, endpoint string) (*Langs, error) {
-	var jsonLangs Langs
 	resp, err := http.Get(server + endpoint)
+	if err != nil {
+		return nil, err
+	} else if resp.StatusCode > 299 {
+		return nil, fmt.Errorf("Response failed with status code: %d\n", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	body, err := io.ReadAll(resp.Body)
-	defer resp.Body.Close()
-
-	//should I just avoid the status code check and just have one single error check here?
-	if resp.StatusCode > 299 {
-		return nil, fmt.Errorf("Response failed with status code: %d\n", resp.StatusCode)
-	} else if err != nil {
-		return nil, err
-	}
+	var jsonLangs Langs
 	json.Unmarshal(body, &jsonLangs)
 	return &jsonLangs, fmt.Errorf("")
 }
 
+/*
 func postSourceFile(server string, endpoint string, filepath string, explicit bool) string {
 	return "placeholder"
 }
+*/
