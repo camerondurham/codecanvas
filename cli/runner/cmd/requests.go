740f40d568f53eea62ff20d6d97922c089cd0313
Arek Ouzounian <agouzo777@gmail.com>
2022-02-01 11:21:54 -1000

    'Run' command preliminary function (#25)
    * Minor changes in langs.go to make it just a bit cleaner.
    Run command working with source file arg parsing.
    Started work on 'lang' flag.
    
    * Fixing dust so linter isn't angry
    
    * Added headers & request timeout, Changed language api call to use an http client rather than http.Get calls. Got rid of unnecessary comments for cleaner codebase

--- cli/runner/cmd/requests.go
+++ cli/runner/cmd/requests.go
@@ -1,17 +1,30 @@
-//file to contain helper functions for different commands
-
 package cmd
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"time"
 )
 
-// Simple struct to hold JSON vals from API call
+const (
+	SERVER        = "http://localhost:8080"
+	RUN_ENDPOINT  = "/api/v1/run"
+	LANG_ENDPOINT = "/api/v1/languages"
+
+	CLIENT_TIMEOUT = 5.0
+)
+
 type Langs struct {
 	Languages []string `json:"languages"`
+}
+
+type StdReturn struct {
+	Stdout string `json:"stdout"`
+	Stderr string `json:"stderr"`
 }
 
 // This function is designed to send an API call to the specified endpoint.
@@ -20,11 +33,20 @@ type Langs struct {
 // This function will return a pointer to the Langs struct which contains a list of the languages
 // currently supported by the API, and an error parameter in the case of failure.
 func getLangListJSON(server string, endpoint string) (*Langs, error) {
-	resp, err := http.Get(server + endpoint)
+	req, err := http.NewRequest("GET", server+endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+	client := &http.Client{
+		Timeout: time.Second * CLIENT_TIMEOUT,
+	}
+	//Add any headers or cookies necessary here
+
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	} else if resp.StatusCode > 299 {
-		return nil, fmt.Errorf("Response failed with status code: %d\n", resp.StatusCode)
+		return nil, fmt.Errorf("Request failed with status code: %d\n", resp.StatusCode)
 	}
 	defer resp.Body.Close()
 
@@ -34,12 +56,64 @@ func getLangListJSON(server string, endpoint string) (*Langs, error) {
 	}
 
 	var jsonLangs Langs
-	json.Unmarshal(body, &jsonLangs)
-	return &jsonLangs, fmt.Errorf("")
+	decodeErr := json.Unmarshal(body, &jsonLangs)
+	panicCheck(decodeErr)
+
+	return &jsonLangs, nil
 }
 
-/*
-func postSourceFile(server string, endpoint string, filepath string, explicit bool) string {
-	return "placeholder"
+// This function is designed to send a POST request to the specified endpoint.
+// The server parameter contains an API host, such as 'http://localhost:8080'
+// The endpoint parameter contains an API endpoint, such as '/api/v1/run'
+// The filepath parameter contains the relative filepath to the source file
+// The explicit parameter tells us whether or not the user would like to
+// have the CLI check the language, or to have the server handle it internally
+func postSourceFile(server string, endpoint string, filepath string, langCheck string) (*StdReturn, error) {
+	source, err := os.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	body := bytes.NewReader(source)
+	req, err := http.NewRequest("POST", server+endpoint, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	//TODO: implement CLI check language logic before adding cookie
+	cookie := http.Cookie{
+		Name:  "language",
+		Value: langCheck,
+	}
+	req.AddCookie(&cookie)
+
+	client := &http.Client{
+		Timeout: time.Second * CLIENT_TIMEOUT,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	} else if resp.StatusCode > 299 {
+		return nil, fmt.Errorf("Request failed with status code: %d\n", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	respReader, readErr := io.ReadAll(resp.Body)
+	if readErr != nil {
+		return nil, err
+	}
+
+	var ret StdReturn
+	decodeErr := json.Unmarshal(respReader, &ret)
+	panicCheck(decodeErr)
+
+	return &ret, nil
 }
-*/
+
+func panicCheck(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
