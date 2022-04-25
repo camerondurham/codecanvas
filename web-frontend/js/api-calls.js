74efa98988c3f7e85b2119e2c1a814b1a19c8641
Arek Ouzounian <agouzo777@gmail.com>
2022-04-24 23:59:51 -0700

    API Request overhaul with promises and asynchronous support. The API's supported languages are queried on load, and stored into a drop-down menu. A small number of themes can be chosen for the editor from a dropdown menu. Minor visual changes.

--- web-frontend/js/api-calls.js
+++ web-frontend/js/api-calls.js
@@ -1,30 +1,74 @@
-var curr_lang = "python3";
+var curr_lang;
 let url = "http://localhost:10100/api/v1/";
 let run_endpoint = "run";
 let lang_endpoint = "languages";
 
-
-function runCall() {
-    var req = {
-        "source": codeMirror.getValue(), 
-        "language": curr_lang,
-    }
-    
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url + run_endpoint, true);
-    xhr.setRequestHeader("Content-type", "application/json");
-    xhr.addEventListener("load", function() {
-        console.log(JSON.parse(xhr.response));
-    }, false);
-    xhr.send(JSON.stringify(req));
-    
+function updateLanguage()
+{
+    var selector = document.getElementById('lang-select');
+    curr_lang = selector.options[selector.selectedIndex].innerText;
 }
 
-function langCall() {
-    var xhr = new XMLHttpRequest(); 
-    xhr.open("GET", url + lang_endpoint, true);
-    xhr.addEventListener("load", function() {
-        console.log(JSON.parse(xhr.response));
-    }, false);
-    xhr.send(); 
+function runRequest() {
+    return new Promise(function(resolve, reject) {
+        var req = {
+            "source": codeMirror.getValue(), 
+            "language": curr_lang
+        }
+        let xhr = new XMLHttpRequest(); 
+        xhr.open("POST", url + run_endpoint);
+        xhr.onload = function() {
+            if (this.status >= 200 && this.status < 400) {
+                resolve(xhr.response);
+            }
+            else {
+                reject({
+                    status: this.status, 
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function() {
+            reject({
+                status: this.status, 
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send(JSON.stringify(req)); 
+    });
+}
+
+async function runCall() {
+    await runRequest()
+        .then(function(result) {
+            console.log(JSON.parse(result));
+        })
+        .catch(function(err) {
+            console.log(err);
+        }); 
+}
+
+function langRequest() {
+    return new Promise(function(resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url + lang_endpoint);
+        xhr.onload = function() {
+            if(this.status >= 200 && this.status < 400) {
+                resolve(xhr.response);
+            }
+            else {
+                reject({
+                    status: this.status, 
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function() {
+            reject({
+                status: this.status, 
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send(); 
+    });
 }
