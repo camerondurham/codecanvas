26401a8b22bec4b1e0a3c73853bc77a3459fff46
cam <u64.cam@gmail.com>
2022-04-30 22:43:11 -0700

    Split code into separate files

---
+++ web-frontend/js/langs-request.js
@@ -0,0 +1,29 @@
+function updateLanguage() {
+  var selector = document.getElementById("lang-select");
+  return selector.options[selector.selectedIndex].innerText;
+}
+
+function langRequest() {
+    return new Promise(function(resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url + lang_endpoint);
+        xhr.onload = function() {
+            if(this.status >= 200 && this.status < 400) {
+                resolve(xhr.response);
+            }
+            else {
+                reject({
+                    status: this.status, 
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function() {
+            reject({
+                status: this.status, 
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send(); 
+    });
+}
