ae59375cc4ae39b6550e7fc7a6c9c8553e471b21
cam <u64.cam@gmail.com>
2022-06-04 21:21:21 -0700

    Add output channel limiter

--- util/iohelpers/helpers.go
+++ util/iohelpers/helpers.go
@@ -8,13 +8,14 @@ import (
 )
 
 // GetWriterChannelOutput gets a string channel from read close to be converted into a string after closer is finished
-func GetWriterChannelOutput(pipeReadCloser io.ReadCloser) chan string {
+func GetWriterChannelOutput(pipeReadCloser io.ReadCloser, bufferMaxSize int64) chan string {
 	outChannel := make(chan string)
 
 	// copy output to separate goroutine so printing can't block indefinitely
 	go func() {
 		var buf bytes.Buffer
-		_, err := io.Copy(&buf, pipeReadCloser)
+		// restrict user from writing more than max buf size 😈
+		_, err := io.CopyN(&buf, pipeReadCloser, bufferMaxSize)
 		if err != nil {
 			print.DebugPrintf("error copying output: %v", err)
 		}
