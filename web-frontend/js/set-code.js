0e545de0d4a1ed0790a3aa5e2660d984d3341827
Cam <17013462+camerondurham@users.noreply.github.com>
2023-10-22 12:43:48 -0700

    Update set-code.js to have more interesting code snippets

--- web-frontend/js/set-code.js
+++ web-frontend/js/set-code.js
@@ -5,7 +5,7 @@ function setLang() {
   const lang = selector.options[selector.selectedIndex].innerText;
   if (lang === "python3" || lang === "python") {
     codeMirror.setValue(
-      "def main():\n\tprint('Hello, World!')\n\nif __name__ == '__main__':\n\tmain()"
+      "def fibonacci(n):\n\tif n<=1:\n\t\treturn n\n\telse:\n\t\treturn(fibonacci(n-1) + fibonacci(n-2))\n\nn = 5\n\nfibo_series = []\n\nfor i in range(0,n):\n\tfibo_series.append(fibonacci(i))\n\nprint(fibo_series)"
     );
   } else if (
     lang === "node" ||
@@ -13,12 +13,27 @@ function setLang() {
     lang === "js" ||
     lang === "javascript"
   ) {
-    codeMirror.setValue("console.log('hello world');");
+    codeMirror.setValue(`const { exec } = require("child_process");
+console.log("Hello world from Node.js! My uptime is:");
+exec("uptime", (error, stdout, stderr) => {
+    if (error) {
+        console.log(\`error: ${error.message}\`);
+        return;
+    }
+    if (stderr) {
+        console.log(\`stderr: ${stderr}\`);
+        return;
+    }
+    console.log(\`stdout: ${stdout}\`);
+});`);
   } else if (lang === "c++" || lang === "cpp" || lang === "c++11") {
     codeMirror.setValue(
       `#include<iostream>
+#include<thread>
 int main() {
-	std::cout << "hello world" << std::endl;
+	unsigned int nthreads = std::thread::hardware_concurrency();
+	std::cout << "hello world from C++!" << std::endl;
+	std::cerr << "I have " << nthreads << " threads!" << std::endl;
 	return 0;
 }`
     );
